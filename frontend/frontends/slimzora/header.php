b41ac665d20681b2b3af1d76e3b8bad37ab56b18
rcarlson <rcarlson@402b1859-9130-0410-a119-97fbf028bc69>
2007-02-08 21:18:17 +0000

    Updated User Agent detection for Windows Mobile Devices - added a small login page for Slimzora for mobile devices

--- frontend/frontends/slimzora/header.php
+++ frontend/frontends/slimzora/header.php
@@ -157,6 +157,126 @@
 		function jzFrontend() {
 			parent::_constructor();
 		}
+		
+		/**
+		* Draws the login page.
+		* 
+		* @author Ben Dodson <bdodson@seas.upenn.edu>
+		* @version 11/3/04
+		* @since 5/13/04
+		*/
+		function loginPage($failed = false) {
+		
+			$display = &new jzDisplay();
+			//$display->preHeader('Login',$this->width,$this->align);
+			
+			echo '<body onLoad="document.getElementById(\'loginform\').field1.focus();"></body>';
+			
+			$urla = array();			
+			$urla['jz_path'] = isset($_GET['jz_path']) ? $_GET['jz_path'] : '';
+			?>
+				<style>
+					body {
+						background-color: #F5F5D0;
+						background: #F5F5D0;
+						font-family: Verdana, Sans;
+						font-size: 10px;
+						color: #9c9b9b;
+						margin: 0 0 0 0;
+					}
+					td {
+						font-family: Verdana, Sans;
+						font-size: 10px;
+					}
+					submit {
+						border: 1px solid black;
+						background: #EFEFCC;
+						color: #000000;
+						font-size: 11px;
+						border-width: 1px;
+					}
+					input {
+						font-family: Verdana, Sans;
+						color: #000000;
+						background-color: #EFEFCC;
+						font-size: 11px;
+						border-width: 1px;
+					}
+				</style>
+				    <script language="javascript" src="lib/md5.js"></script>
+				    <script language="javascript">
+				    function submitLogin() {
+				      if (document.getElementById("loginform").doregister.value == 'true') {
+					return true;
+				      } else {
+					// submit the other form
+					// so we can submit a non-cleartext PW without changing browser's stored PW.
+					document.getElementById("loginSecureForm").field1.value = 
+					         document.getElementById("loginform").field1.value;
+
+					document.getElementById("loginSecureForm").field2.value = 
+					hex_md5(document.getElementById("loginform").field2.value);
+
+					document.getElementById("loginSecureForm").remember.value =
+					document.getElementById("loginform").remember.value;
+
+					document.getElementById("loginSecureForm").submit();
+					return false;
+				      }
+				    }
+				    </script>
+				<body style="background-color: #F5F5D0;">
+				<table width="100%" height="100%" cellpadding="0" cellspacing="0" border="0" style="background-color: #F5F5D0;">
+					<tr>
+						<td align="center" height="100%" width="100%" style="background-color: #F5F5D0;">
+							<?php
+								if ($failed) {
+									echo "<center><strong><font color=white>Incorrect password</font></strong></center>";
+								}
+							?>
+							<form name="loginSecureForm" id="loginSecureForm" method="POST" action="<?php echo urlize($urla); ?>">
+								<input type="hidden" name="field1" value="">
+								<input type="hidden" name="field2" value="">
+	                            <input type="hidden" name="remember" value="">
+								<input type="hidden" name="<?php echo jz_encode('action'); ?>" value="<?php echo jz_encode('login'); ?>">
+                            </form>
+							<form name="loginform" id="loginform" method="POST" action="<?php echo urlize($urla); ?>" onsubmit="return submitLogin()">
+								<input type="hidden" name="<?php echo jz_encode('action'); ?>" value="<?php echo jz_encode('login'); ?>">
+								<?php
+									if (!$failed) {
+										echo "<br><br><br>";
+									}
+								?>
+								<?php echo word("Username"); ?><br>
+								<input size="18" type="text" class="jz_input" name="field1" style="width:146px;">
+								<br>
+								<?php echo word("Password"); ?><br>
+								<input size="18" type="password" class="jz_input" name="field2" style="width:146px;">
+								<br>
+								<input type="checkbox" class="jz_checkbox" name="remember"> <?php echo word("Remember me"); ?>
+								<br><br>
+								<input class="jz_submit" type="submit" name="<?php echo jz_encode('submit_login'); ?>" value="<?php echo word("Login"); ?>">
+								   <input type="hidden" name="doregister" value="false" />
+								<?php $be = new jzBackend();
+									$data = $be->loadData('registration');
+									if ($data['allow_registration'] == "true") {
+									?>
+										&nbsp;<input class="jz_submit" type="submit" name="<?php echo jz_encode('self_register'); ?>" value="<?php echo word("Register"); ?>" onclick="document.getElementById('loginform').doregister.value='true'">
+									<?php 
+									} 
+									?>
+
+							</form>
+							<br /><br />
+							<img src="style/images/login-footer-logo.gif" border="0">
+							<br /><br />
+						</td>
+					</tr>
+				</table>
+				</body>
+			<?php
+			//this->footer();
+		}
 			
 		function pageTop($title) {
 			global $img_up_arrow, $row_colors;
--- frontend/useragent.php
+++ frontend/useragent.php
@@ -8,8 +8,17 @@
 	
 	$useragent = $_SERVER['HTTP_USER_AGENT'];
 	
+	// This is for the Nintendo Wii
 	if (false !== stristr($useragent, 'Nintendo Wii')) {
 		define('JZ_FRONTEND_OVERRIDE','wiijay');
 	}
 	
+	// This is for Windows Mobile Devices
+	if (false !== stristr($useragent, 'Windows CE')) {
+		define('JZ_FRONTEND_OVERRIDE','slimzora');
+		define('JZ_STYLE_OVERRIDE','sandstone');
+		global $jzSERVICES;
+		$jzSERVICES->loadService('players','ptunes');
+	}
+		
 ?>
