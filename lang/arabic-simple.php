3efc8373d2659203f168ce8dad12cdd9ba7a85e3
bdodson <bdodson@402b1859-9130-0410-a119-97fbf028bc69>
2007-05-31 21:45:11 +0000

    Adding arabic (simple) lang file- thanks Sami!

---
+++ lang/arabic-simple.php
@@ -0,0 +1,131 @@
+<?php 
+        if (!defined(JZ_SECURE_ACCESS)) die ('Security breach detected.');
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *   
+        * JINZORA | Web-based Media Streamer  
+        *
+        * Jinzora is a Web-based media streamer, primarily desgined to stream MP3s  
+        * (but can be used for any media file that can stream from HTTP).  
+        * Jinzora can be integrated into a PostNuke site, run as a standalone application, 
+        * or integrated into any PHP website.  It is released under the GNU GPL.
+        * 
+        * Jinzora Author:
+        * Ross Carlson: ross@jasbone.com
+        * http://www.jinzora.org
+        * Documentation: http://www.jinzora.org/docs        
+        * Support: http://www.jinzora.org/forum
+        * Downloads: http://www.jinzora.org/downloads
+        * License: GNU GPL <http://www.gnu.org/copyleft/gpl.html>
+        * 
+        * Contributors:
+        * Please see http://www.jinzora.org/modules.php?op=modload&name=jz_whois&file=index
+        * Translated by Sami Al-Nawasreh www.nawasreh.com
+        * ÊÚÑíÈ ÓÇãí ÇáäæÇÕÑå s@nawasreh.com
+        * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+        $words['f2a6c498fb90ee345d997f888fce3b18'] = "ãÓÍ";
+        $words['801ab24683a4a8c433c6eb40c48bcd9d'] = "ÊÍãíá";
+        $words['82609dd1953ccbfbb4e0d20623193b56'] = "ÑÌæÚ";
+        $words['d3da97e2d9aee5c8fbe03156ad051c99'] = "ÇáãÒíÏ";
+        $words['de3c731be5633838089a07179d301d7b'] = "ÅÓÊãÇÚ";
+        $words['a7cf13d72ea95142d1d1da49ccf92ab1'] = "ÅÓÊãÇÚ ÚÔæÇÆí";
+        $words['c0a9132468f09f5265b21c53d95ef05c'] = "ÅÒÇÍÉ ÇáãáÝ";
+        $words['db0d97a3e139cb7fdd87de1cc38ea505'] = "ÏÑÌå Çáì ÃÚáì";
+        $words['bfb6800020a06f5986570499d33f3ce5'] = "ÞÇÆãÉ ÅáÇÓÊãÇÚ";
+        $words['dcb66ff6e4a2517ade22183779939c9d'] = "ÊÞíã";
+        $words['c7df65dda878ac02ecdceea042185df7'] = "äÞÇÔ";
+        $words['dc30bc0c7914db5918da4263fce93ad2'] = "ãÓÍ";
+        $words['910885435dbc44a22b68cb45c79e4a7e'] = "ÇÖÝ Çáì ÇáãÝÖáå";
+        $words['ffcf0876cde0a73d4ac86737d0520e83'] = "ÅÓÊãÚ ááæÝí";
+        $words['03c2e7e41ffc181a4e84080b4710e81e'] = "ÌÏíÏ";
+        $words['b3faff8212534facf5289900a5c7d635'] = "ááÊÈÇÏá ÇáÈÑíÏí";
+        $words['01fc81f4b0050806770e5af5927f1b2e'] = "ÅÓÊãÚ áãÞØÚ";
+        $words['b7d7436a18bf546dd6438d92218868c1'] = "ÅÝÍÕ Çáßá";
+        $words['e15b8c32d9b14e4a21db040c1e691335'] = "áÇ ÊÝÍÕ";
+        $words['105b296a83f9c105355403f3332af50f'] = "ÅíÞÇÝ ãÄÞÊ";
+        $words['11a755d598c0c417f9a36758c3da7481'] = "ÊæÞíÝ";
+        $words['dd1f775e443ff3b9a89270713580a51b'] = "ÇáÓÇÈÞ";
+        $words['10ac3d04253ef7e1ddc73e6091c0cd55'] = "ÇáÊÇáí";
+        $words['0bfc42f23de82943797f49b19734a181'] = "ÅÔÊÑÇß";
+        $words['4798cc774d4e385a2e87e911a1be6378'] = "ÇáÇÞÓÇã";
+        $words['b046ed95f719df2e79c9cb3e3bc23bf1'] = "ÏÎáÊ ÈÜ";
+        $words['0323de4f66a1700e2173e9bcdce02715'] = "ÎÑæÌ";
+        $words['4cd11bfc07c2b3fda63d7eff9de2ae15'] = "ÅÚÏÇÏÇÊ";
+        $words['ff8e4051149fdc4d61bfbd5031025bdf'] = "ÌÇÑí ÇáÇä";
+        $words['6007d31080a0d15cb50d548ef94be2b8'] = "ãä æÃíä";
+        $words['13348442cc6a27032d2b4aa28b75a5d3'] = "ÈÍË";
+        $words['74b3df98d5d1417597328786482b6bad'] = "ÌãíÚ ÇáÇäÓÇÞ";
+        $words['b7b190b16591f6e62e74d485e5bcd6cd'] = "ÇáÝäÇä";
+        $words['15bf55225283c1c1895f406cd0c6b27b'] = "ÇáÈæã";
+        $words['259e64c61d5d1036664ae826072cafc0'] = "ÇáÚäæÇä";
+        $words['9cf93757e76ec1ffdd8790ce3b74572e'] = "ÇáäÕ";
+        $words['008e13b05685ac886b4bfdf8dbeef32f'] = "ÞÇÆãå ÅÇáÇÓÊãÇÚ";
+        $words['cd7e99b7b09f0845ccd66756b6a836ea'] = " - ÞÇÆãÉ ÇáÇÓÊãÇÚ ÇáÏæÑíå - ";
+        $words['2d909feee3933b3e22cb380790dcd10f'] = "ßæä ÞÇÆãå ÅÇÓÊãÇÚ";
+        $words['00111f0fa1e4ed4c9088aa159d653a94'] = "ÇáÑÌÇÁ ÊÓãíÉ ÞÇÆãÉ ÇáÇÓÊãÇÚ";
+        $words['96d8ac035792e45b2139a7279e4df2b3'] = "ÊÕÍÝ";
+        $words['1b9abf135ccb7ed2f2cd9c155137c351'] = "ÊÕÍÝ";
+        $words['49ae866482bea54ec6c9478a1c1bc17f'] = "ÌãíÚ ÇáÇÞÓÇã";
+        $words['cff65cba30840af244dff4f15819c22e'] = "ÌãíÚ ÇáÝäÇäíä";
+        $words['e8625dd79249fcbcb1483ec0ae4caaac'] = "ÌãíÚ ÇáÈæãÇÊ";
+        $words['2601f56b59bc7852c176835d2cc4ee6f'] = "ÌãíÚ ÇáÚäÇæíä";
+        $words['dae8ace18bdcbcc6ae5aece263e14fe8'] = "ÎíÇÑÇÊ";
+        $words['ddf8099b5bf1814b9728b5752e652cb1'] = "ÅÎÊÑ ãä ÝÖáß...";
+        $words['0149d44b20f21815ecc4d925b39235d8'] = "ÅÎÊíÇÑ ÚÔæÇÆí";
+        $words['5285a27199350c7f28b85073959813ca'] = "ÇáÞÇÆãå ÇáÇÈÌÏíå (ÇáÝäÇäíä)";
+        $words['687c1324d70c437e60d7c07541bee75b'] = "ÊÕÍÝ: ";
+        $words['7a1af30a5ce889031cbc2472766159e3'] = "ÃíÇã ÓÇÈÞå";
+        $words['2362763d9adbdbcf7515e177ba8d6c26'] = "ÇáæÞÊ ÇáÍÇáí";
+        $words['93900eefcbe5ad742413dfbdb9dccb19'] = "ÇáÈæãÇÊ ÚÔæÇÆíå";
+        $words['c5a008f674ccff04a17d0821efe39fda'] = "ÔÇåÏ ÌãíÚ ÇáÝäÇäíä -ÇáÈæãÇÊ";
+        $words['4dae6e4cfffdbd82fdd3b6eb3bb4bee9'] = "ÔÇåÏ ÇáÈæã";
+        $words['bacc02ea491b1609d92b701e6bd8f6a6'] = "ÅÓÊãÚ ááÎíÇÑ";
+        $words['2269ebf830e62014ebec46b829529f4c'] = "ãÚáæãÇÊ ÇáÝäÇä";
+        $words['a9d841babe1bf999b6ee4eb818e0dbc5'] = "ÔÇåÏ ÌãíÚ ÇáãÓÇÑÇÊ áÜ";
+        $words['5bca2f066df1fe641b2b779db2a63c0a'] = "Úíäå";
+        $words['fc5a57d1d8ec78257a313990839c28f5'] = "ÇáÈæã ÝäÇä";
+        $words['331bcb09f82e8cd61f81b769858e6c4b'] = "Jinzora ÑÇÏíæ";
+        $words['1a6177050a05241df35705cc43365746'] = "ÝäÇä ããÇËá";
+        $words['78945de8de090e90045d299651a68a9b'] = "ÕÇáÍ ááÇÖÇÝå";
+        $words['67c19e107de33cab7ea9a9db8bc9ccd2'] = "áÇ íÕáÍ ááÇÖÇÝå";
+        $words['178df7ae3ec428e6f98afc55738dae17'] = "ÃÖÝ Çáì";
+        $words['45b963397aa40d4a0063e0d85e4fe7a1'] = "ãáÝÇÊ";
+        $words['7cb31435da1bef1e450754ff725b0fdd'] = "ãáÝ";
+        $words['d0834fcec6337785ee749c8f5464f6f6'] = "ÅÚÏÇÏÇÊ";
+        $words['5f075ae3e1f9d0382bb8c4632991f96f'] = "ÇÐåÈ";
+        $words['8cf04a9734132302f96da8e113e80ce5'] = "ÇáÑÆíÓíå";
+        $words['aab37d1ffe6a5c78ac1b0453298336a5'] = "ÇÓÊãÇÚ áÌãíÚ ÇáÈæãÇÊ ";
+        $words['a6546983e21e28532ef1b62dd1c2f342'] = "ÇÓÊãÇÚ ÚÔæÇÆí áÌãíÚ ÇáÇáÈæãÇÊ";
+        $words['91b442d385b54e1418d81adc34871053'] = "ÇÎÊíÑÊ";
+        $words['c11c03a2d1667c75c9bc2f95e5431b6d'] = "ÞÇÆãÉ ÇáÇÓÊãÇÚ ÇáÏæÑíå";
+        $words['bd5364495054455bf007f88e032a4a9e'] = "ÞÇÆãÉ ÅÓÊãÇÚ ÌÏíÏå";
+        $words['db6c7f760e932f7d18538df3bbc42d47'] = "ÅÏÇÑÉ ÇáÇäÓÇÞ";
+        $words['06df33001c1d7187fdd81ea1f5b277aa'] = "ÇÝÚÇá";
+        $words['e4e9c5d5e1740efba2f411ce9ed14141'] = "ÅÚÇÏÉ ßÊÇÈÉ ÇáÈØÇÞå";
+        $words['faf42e63efca7712ccd20564d6a5dbce'] = "ÊÍãíá ÇáÈæã";
+        $words['2cf3728d01219eeff02feab45380d454'] = "ããíÒÇÊ ÇáãÌãæÚÇÊ";
+        $words['759777e1cc192fac9327409439f027a8'] = "ÅÓÊãÇÚ áÇáÈæã";
+        $words['4bdd53cd2836fc609523468fc1e5e63e'] = "ÇáÇÞÑÇÕ";
+        $words['60d65769e17d580b4cc077b24ba2b1dd'] = "ãÚáæãÇÊ ÇáãÇÏå";
+        $words['e200177263ab3bf3469abe6d5756b729'] = "ÊÛíÑ ÝäÇä";
+        $words['6eafa2d3bda1939b5d9069079b20601c'] = "ÊÕÍÝ ÇáÇáÈæãÇÊ";
+        $words['c61232ced6bb593e4c9b645dda49ca88'] = "ÊÚÏíá ÇáãÚáæãÇÊ";
+        $words['8298b49041b2e2937bc1bbbffff88c00'] = "ÊÑÞíÉ ÇáãÎÒä";
+        $words['4200bec1a461b2488f4fc69e208ae508'] = "ÅÙåÇÑ ÇáãÎÝí";
+        $words['c72510c7223aacdeff4be54fcb96c608'] = "ÊÚÏíá ÌãÇÚí";
+        $words['12400803aab67b8428f2afe8c86d786c'] = "ÅÙåÇÑ ÇáäÕ ";
+        $words['4818f5bf58af4ce256ae7b5d1193eeb4'] = "ÇÙåÑ ßá ÚäÇæíä";
+        $words['97b938f50294a7820b72eaf44da773b3'] = "ID3-ááÈØÇÞÇÊ ßÊÇÈÉ ÌÏíÏå";
+        $words['064e60c314c83fcdfc477bbc05dc7512'] = "ÊÚÏíá ãÚáæãÇÊ ÇáÈæã";
+        $words['e81bea82d2387d2ae9081f3d523f9e09'] = "ÇÖÝ ÝäÇä ãÔÇÈå";
+        $words['86b824abc4ee818316716660cbca2746'] = "ÃÖÝ ãÓÇÑ æåãí";
+        $words['6f6cb72d544962fa333e2e34ce64f719'] = "ÇáÍÌã";
+        $words['700a01983c221effeb06e8d7faa6e8dd'] = "ãÚÏá ÇáÓÑÚå";
+        $words['f579f693ac9ecb3fb9e822bf32028e13'] = "ãÚÏá ÇáÚíäå";
+        $words['ba2a9c6c8c77e03f83ef8bf543612275'] = "ÇáØæá";
+        $words['a176773b5b381cf45ef8fe47885a6307'] = "ÑÞã ÇáãÓÇÑ";
+        $words['df58501f694a52d2b720ce73d977c199'] = "ÃÓÊãÚ ááÌãíÚ ÚÔæÇÆí";
+        $words['4287655f5e50f02784b0d83bfc9c25a9'] = "ÃÖÝ ÇáÌãíÚ";
+        $words['a74ed2292e5193d1c02f49259df3be1b'] = "ÌãíÚ ÇáÇÞÓÇã";
+        $words['c5b4555e9cce389a90626cb3cbad03ac'] = "ÝäÇä ÌÏíÏ";
+        $words['6413728c55bd3515a9f787ef2edb52bc'] = "ÇáÈæãÇÊ ÌÏíÏå";
+        $words['9f7be70d63e234020f15b5782c6f0c5f'] = "ÌãíÚ ÇáÝäÇäíä";
+?>
