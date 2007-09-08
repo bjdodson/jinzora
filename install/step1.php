164420002587a10c9b47f56c4c50ef0782be3040
maarten <maarten@402b1859-9130-0410-a119-97fbf028bc69>
2007-09-08 17:22:17 +0000

    Finished testing the new SVN Changelog

--- install/step1.php
+++ install/step1.php
@@ -2,7 +2,7 @@
 	// Let's figure out the path stuff so we'll know how/where to include from
 	$form_action = setThisPage() . "install=step2";
 
-	// Now let's include the left TEST
+	// Now let's include the left
 	include_once($include_path. 'install/leftnav.php');
 
 	// Let's set a session variable so we can test for session support
