e58383639d807bb36fea705d3f2ea3c3d8ff86ff
bdodson <bdodson@402b1859-9130-0410-a119-97fbf028bc69>
2007-10-30 22:04:02 +0000

    updated icons

--- style/css.php
+++ style/css.php
@@ -4,6 +4,73 @@
 		define("jz_font_size","11px");	
 	}
 ?>
+
+#jz_images img.icon{
+	background: url('<?php echo $root_dir; ?>/style/<?php echo $skin; ?>/icons.gif') no-repeat;
+	width: 17px;
+	margin: 0px;
+	vertical-align: bottom;
+}       
+#jz_images img.icon-play{background-position: 0px 0px;}
+#jz_images img.icon-random{background-position: -16px 0px;}
+#jz_images img.icon-download{background-position: -32px 0px;}
+#jz_images img.icon-prefs{background-position: -48px 0px;}
+#jz_images img.icon-home{background-position: -64px 0px;}
+#jz_images img.icon-clip{background-position: -80px 0px;}
+#jz_images img.icon-check{background-position: -96px 0px;}
+#jz_images img.icon-nocheck{background-position: -112px 0px;}
+#jz_images img.icon-add{background-position: -128px 0px;}
+#jz_images img.icon-more{background-position: -144px 0px;}
+#jz_images img.icon-tools{background-position: -160px 0px;}
+#jz_images img.icon-slimpop{background-position: -176px 0px;}
+#jz_images img.icon-play-disabled{background-position: -192px 0px;}
+#jz_images img.icon-random-disabled{background-position: -208px 0px;}
+#jz_images img.icon-download-disabled{background-position: -224px 0px;}
+#jz_images img.icon-tiny-play{background-position: -240px 0px; width: 11px; height: 12px; vertical-align: top;}
+#jz_images img.icon-tiny-play-dis{background-position: -250px 0px; width: 11px; height: 12px; vertical-align: top;}
+#jz_images img.icon-tiny-info{background-position: -260px 0px; width: 11px; height: 12px; vertical-align: top;}
+#jz_images img.icon-playlist{background-position: -272px 0px;}
+#jz_images img.icon-rate{background-position: -288px 0px;}
+#jz_images img.icon-discuss{background-position: -304px 0px;}
+#jz_images img.icon-discuss-disabled{background-position: -320px 0px;}
+#jz_images img.icon-email{background-position: -336px 0px;}
+#jz_images img.icon-pause{background-position: -352px 0px;}
+#jz_images img.icon-stop{background-position: -368px 0px;}
+#jz_images img.icon-previous{background-position: -384px 0px;}
+#jz_images img.icon-next{background-position: -400px 0px;}
+#jz_images img.icon-clear{background-position: -416px 0px;}
+#jz_images img.icon-repeat{background-position: -432px 0px;}
+#jz_images img.icon-norepeat{background-position: -448px 0px;}
+#jz_images img.icon-rss{background-position: -464px 0px; width: 23px; height: 12px; vertical-align: top;}
+#jz_images img.icon-purchase{background-position: -490px 0px;}
+#jz_images img.icon-podcast{background-position: -506px 0px;}
+#jz_images img.icon-uparrow{background-position: -522px 0px; width: 15px;}
+#jz_images img.icon-star-h-e{background-position: -538px 0px; width: 9px; height: 16px; vertical-align: bottom;}
+#jz_images img.icon-star-f-e{background-position: -548px 0px; width: 9px; height: 16px; vertical-align: bottom;}
+#jz_images img.icon-star-r{background-position: -559px 0px; width: 3px; height: 16px; vertical-align: bottom;}
+#jz_images img.icon-star-l{background-position: -563px 0px; width: 3px; height: 16px; vertical-align: bottom;}
+#jz_images img.icon-star-f{background-position: -567px 0px; width: 9px; height: 16px; vertical-align: bottom;}
+
+.jz_main_block_topl {
+	background: url('<?php echo $root_dir; ?>/style/<?php echo $skin; ?>/icons.gif') no-repeat;
+	background-position: left;
+	width: 5px;
+	margin: 0px;
+	vertical-align: bottom;
+	background-position: 0px -27px;
+}
+.jz_main_block_topr {
+	background: url('<?php echo $root_dir; ?>/style/<?php echo $skin; ?>/icons.gif') no-repeat;
+	background-position: left;
+	width: 5px;
+	margin: 0px;
+	vertical-align: bottom;
+	background-position: -7px -27px;
+}
+
+
+
+
 #slickLeftNav{
 	float: left;
 }
@@ -170,19 +237,9 @@ body {
 	border: 1px solid #474747; 
 	background-color: <?php echo jz_bg_color; ?>;
 }
-.jz_main_block_topl {
-	background: url("<?php echo $root_dir; ?>/style/<?php echo $skin; ?>/sect-header-left.gif"); 
-	background-repeat:no-repeat;
-	background-position: left;
-}
 .jz_main_block_topm {
 	background: url("<?php echo $root_dir; ?>/style/<?php echo $skin; ?>/sect-header-middle.gif"); 
 	background-repeat: repeat;
-}
-.jz_main_block_topr {
-	background: url("<?php echo $root_dir; ?>/style/<?php echo $skin; ?>/sect-header-right.gif"); 
-	background-repeat:no-repeat;
-	background-position: right;
 }
 td {
 	font-family: Verdana, Sans;
