700795e587324fa4c139f681e3c4cc14ce8fa1e6
bdodson <bdodson@402b1859-9130-0410-a119-97fbf028bc69>
2007-10-31 22:02:37 +0000

    icon fix

--- frontend/frontends/andro/blocks/playlist-bar.php
+++ frontend/frontends/andro/blocks/playlist-bar.php
@@ -1,4 +1,5 @@
 <?php if (!defined(JZ_SECURE_ACCESS)) die ('Security breach detected.');
+global $img_playlist;
 	if (sizeof($nodes) > 0 || sizeof($tracks) > 0) {
 		$smarty->assign('addListButton', $display->addListButton(true));				
 		$smarty->assign('hidden_1', $display->hiddenVariableField('action','mediaAction'), true, true);
@@ -6,7 +7,7 @@
 		$url_array = array();
 		$url_array['action'] = "popup";
 		$url_array['ptype'] = "playlistedit";
-		$smarty->assign('playlist_button', '<a href="javascript:;" onClick="openPopup('. "'". urlize($url_array). "'". ',600,600); return false;"><img src="'. $image_dir. 'playlist.gif" border="0"></a>');
+		$smarty->assign('playlist_button', '<a href="javascript:;" onClick="openPopup('. "'". urlize($url_array). "'". ',600,600); return false;">'.$img_playlist.'</a>');
 		$smarty->assign('playlist_select', $display->playlistSelect(115, false, "all", true, "jz_playlist", true));
 		$smarty->assign('playlist_play_button',$display->playListButton(true));
 		$smarty->assign('playlist_random_button',$display->randomListButton(true));
