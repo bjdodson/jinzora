bb0aaa758d8c335798aefdde3fe48d5248893f47
bdodson <bdodson@402b1859-9130-0410-a119-97fbf028bc69>
2008-01-03 15:23:43 +0000

    scratch work for playlists

--- frontend/frontends/mobile/models/playlist.php
+++ frontend/frontends/mobile/models/playlist.php
@@ -1,8 +1,22 @@
 <?php if (!defined(JZ_SECURE_ACCESS)) die ('Security breach detected.');
 
 function doTemplate($node) {
+  global $jzUSER;
   $display = &new jzDisplay();
   $smarty = mobileSmarty();
+
+  if (!isset($_SESSION['jz_playlist_queue'])) {
+    jzTemplate($smart,'playlist');
+    return;
+  }
+
+  $elements = array();
+  $pl = $jzUSER->loadPlaylist($_SESSION['jz_playlist_queue']);
+  $list = $pl->getList();
+  foreach ($list as $el) {
+    $elements[] = array('name'=>$el->getName());
+  }
+  $smarty->assign('elements',$elements);
  
   jzTemplate($smarty,'playlist');
 }
