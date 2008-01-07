a5d387f6f63bc6f2c14d83c6775661ebaeff4ddd
bdodson <bdodson@402b1859-9130-0410-a119-97fbf028bc69>
2008-01-07 15:16:39 +0000

    basic jukebox controls

--- frontend/frontends/mobile/models/jukebox.php
+++ frontend/frontends/mobile/models/jukebox.php
@@ -1,9 +1,52 @@
 <?php if (!defined(JZ_SECURE_ACCESS)) die ('Security breach detected.');
 
 function doTemplate($node) {
+  global $jzUSER;
+
   $display = &new jzDisplay();
   $smarty = mobileSmarty();
- 
+
+  $jb = new jzJukebox();
+  if (!$jb->connect()) {
+    //jzTemplate($smarty,'jukeboxError');
+    echo 'Error connecting to jukebox.';
+    return;
+  }
+
+  $smarty->assign('Play',word('Play'));
+  $smarty->assign('Pause',word('Pause'));
+  $smarty->assign('Stop',word('Stop'));
+  $smarty->assign('Previous',word('Previous'));
+  $smarty->assign('Next',word('Next'));
+  $smarty->assign('Shuffle',word('Shuffle'));
+  $smarty->assign('Clear',word('Clear'));  
+
+  if (checkPermission($jzUSER, "jukebox_admin")) {
+    $func = $jb->jbAbilities();
+
+    if ($func['playbutton']) {
+      $smarty->assign('openPlayTag',$display->getOpenJukeboxActionTag('play'));
+    }
+    if ($func['pausebutton']) {
+      $smarty->assign('openPauseTag',$display->getOpenJukeboxActionTag('pause'));
+    }
+    if ($func['stopbutton']) {
+      $smarty->assign('openStopTag',$display->getOpenJukeboxActionTag('stop'));
+    }
+    if ($func['prevbutton']) {
+      $smarty->assign('openPrevTag',$display->getOpenJukeboxActionTag('previous'));
+    }
+    if ($func['nextbutton']) {
+      $smarty->assign('openNextTag',$display->getOpenJukeboxActionTag('next'));
+    }
+    if ($func['shufflebutton']) {
+      $smarty->assign('openShuffleTag',$display->getOpenJukeboxActionTag('random_play'));
+    }
+    if ($func['clearbutton']) {
+      $smarty->assign('openClearTag',$display->getOpenJukeboxActionTag('clear'));
+    }
+  }
+
   jzTemplate($smarty,'jukebox');
 }
 
--- frontend/frontends/mobile/templates/jukebox.tpl
+++ frontend/frontends/mobile/templates/jukebox.tpl
@@ -1,1 +1,30 @@
-jukebox
+{literal}
+<style type="text/css">
+#jbButtons {
+  text-align:center;
+  margin: 10px 10px 10px 15px;
+}
+
+#jbButtons a {
+  font-weight:bold;
+}
+</style>
+{/literal}
+
+<div id="jbButtons">
+{if $openPlayTag}
+{$openPlayTag}>{$Play}</a>
+{/if}
+{if $openPauseTag}
+{$openPauseTag}>{$Pause}</a>
+{/if}
+{if $openStopTag}
+{$openStopTag}>{$Stop}</a>
+{/if}
+{if $openPrevTag}
+{$openPrevTag}>{$Previous}</a>
+{/if}
+{if $openNextTag}
+{$openNextTag}>{$Next}</a>
+{/if}
+</div>
