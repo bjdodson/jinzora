c838d9cfda1bc2088693b45a79d9a958986632a0
bdodson <bdodson@402b1859-9130-0410-a119-97fbf028bc69>
2008-11-16 00:35:11 +0000

    fix for almost lost api changes\!

--- api.php
+++ api.php
@@ -58,7 +58,13 @@
 	$this_page = setThisPage();
         $enable_page_caching = "false";
 
-	
+        $api_page = $this_site.$_SERVER['PHP_SELF'] .'?';
+        if (isset($_REQUEST['user'])) {
+	  $api_page .= 'user='.urlencode($_REQUEST['user']);
+	}
+        if (isset($_REQUEST['pass'])) {
+	  $api_page .= '&pass='.urlencode($_REQUEST['pass']);
+	}
 	
 	
 	// Let's create our user object for later
@@ -99,7 +105,7 @@
         if (empty($_REQUEST['request']) && (isset($_REQUEST['query']) || isset($_REQUEST['search'])))  {
 	  $_GET['request'] = 'search';
 	}
-	
+
 	// Now let's see what they want
 	switch($_GET['request']){
 		case "genres":
@@ -117,6 +123,14 @@
 		break;
 		case "search":
 			return search();
+	        case "browse":
+	                return browse();
+		break;
+	        case "home":
+		  return home();
+		break;
+	        case "chart":
+		  return chart();
 		break;
 		case "stylesheet":
 			echo '<link rel="stylesheet" title="slick" type="text/css" media="screen" href="'. $root_dir. '/style/'. $skin. '/default.php">';
@@ -157,6 +171,23 @@
 	}
 	
 	
+
+/**
+ * Gets the output format requested
+ *
+ * @author Ben Dodson
+ * @since 6/21/08
+ */
+function getFormatFromRequest() {
+  if (isset($_REQUEST['type'])){
+    $type = $_REQUEST['type'];
+  } else if (isset($_REQUEST['output'])) {
+    $type = $_GET['output'];
+  } else {
+    $type = "xml";
+  }
+  return $type;
+}
 	
 	
 	// These are the functions for the API
@@ -174,13 +205,7 @@
 		global $jzUSER, $this_site, $root_dir;
 		
 		// What kind of output?
-		if (isset($_REQUEST['type'])){
-			$type = $_REQUEST['type'];
-		} else if (isset($_REQUEST['output'])) {
-		        $type = $_GET['output'];
-		} else {
-			$type = "xml";
-		}
+		$type = getFormatFromRequest();
 		
 		// Let's setup our objects
 		// The display object is just a set of functions related to display
@@ -212,165 +237,82 @@
 			}
 		}
 		*/
-		$tracks = array();
-		$nodes = array();
-		// Now let's break our nodes and tracks out
-		foreach ($results as $val) {
-			// We look at objects as leafs or nodes
-			// Leafs are the last branch on the tree
-			// So those would be tracks/videos
-			if ($val->isLeaf()) {
-				$tracks[] = $val;	
-			} else {
-				// Nodes are everything above the leafs
-				// So albums, artists, and genres
-				$nodes[] = $val;
-			}
-		}
+		print_results($results,$type);
+	}
+
+	/**
+	* 
+	* Browses Jinzora given the path to a node.
+	*
+	* @author Ben Dodson
+	* @since 4/21/05
+	* @version 6/21/08
+	* 
+	**/
+	function browse(){
+		global $jzUSER, $this_site, $root_dir;
 		
-		// Now let's output
-		switch ($type){
-			case "xml":
-				echoXMLHeader();
-				echo "  <search>\n";
-				echo "    <tracks>\n";
-				// Now let's display the tracks
-				if (sizeof($tracks) > 0)
-				foreach($tracks as $track){
-					// Let's get all the data for display
-					// The getMeta function lets us get all the metadata (length, bitrate, etc) from a tack
-					$meta = $track->getMeta();
-					
-					// Now we go up from this item to get it's "ancestors" 
-					// The reason we do this is to make sure we get the right thing
-					// for it, not just the one above.  This is important when using multidisk
-					// albums where their parent would be DISC1 not AlbumName
-					// You can do this recursively if you want
-					$album = $track->getAncestor("album");
-					$artist = $album->getAncestor("artist");
-					$genre = $artist->getParent();
-					
-					// Now let's display
-					echo "      <track>\n";
-					echo "        <name>". xmlentities($meta['title']). "</name>\n";
-					echo "        <metadata>\n";
-					echo "          <filename>". xmlentities($meta['filename']). "</filename>\n";
-					echo "          <tracknumber>". xmlentities($meta['number']). "</tracknumber>\n";
-					echo "          <length>". xmlentities($meta['length']). "</length>\n";
-					echo "          <bitrate>". xmlentities($meta['bitrate']). "</bitrate>\n";
-					echo "          <samplerate>". xmlentities($meta['frequency']). "</samplerate>\n";
-					echo "          <filesize>". xmlentities($meta['size']). "</filesize>\n";
-					echo "        </metadata>\n";
-					echo "        <album>". xmlentities($album->getName()). "</album>\n";
-					echo "        <artist>". xmlentities($artist->getName()). "</artist>\n";
-					echo "        <genre>". xmlentities($genre->getName()). "</genre>\n";
-					echo "        <playlink>". xmlentities($this_site.$track->getPlayHREF()). "</playlink>\n";
-					echo "      </track>\n";
-				}
-				echo "    </tracks>\n";
-				echo "    <nodes>\n";
-				// Now let's display the nodes
-				if (sizeof($nodes) > 0)
-				foreach($nodes as $node){
-					// We do the same things here by getting item off the node
-					// $art would be the image for the item we're looking at
-					// In this case we want the art for the match we found
-					// This works on ALL objects if they have art
-					$art = $node->getMainArt();
-					
-					$album = $node->getAncestor("album");
-					if ($album) {
-						$artist = $album->getAncestor("artist");
-					}
-					
-					echo "      <node>\n";
-					echo "        <name>". xmlentities($node->getName()). "</name>\n";
-					echo "        <type>". xmlentities($node->getPType()). "</type>\n";
-					echo "        <link>". xmlentities($this_site.$display->link($node,false,false,false,true,true)). "</link>\n";
-					if (!empty($album)) {
-					  echo "        <album>". xmlentities($album->getName()). "</album>\n";
-					}
-					if (!empty($artist)) {
-					  echo "        <artist>". xmlentities($artist->getName()). "</artist>\n";
-					}
-					echo "        <image>";
-					if ($art){
-						echo xmlentities($display->returnImage($art,false,false, false, "limit", false, false, false, false, false, "0", false, true, true));
-					}
-					echo "        </image>\n"; 
-					echo "        <thumbnail>";
-					$art = $node->getMainArt('75x75');
-					if ($art){
-						echo xmlentities($display->returnImage($art,false,75,75, "limit", false, false, false, false, false, "0", false, true, true));
-					}
-					echo "        </thumbnail>\n"; 
-					echo "        <playlink>". xmlentities($this_site.$node->getPlayHREF()). "</playlink>\n";
-					echo "      </node>\n";
-				}
-				echo "    </nodes>\n";
-				echo "  </search>\n";
-				echoXMLFooter();
-			break;
-			case "display":
-				// Ok, let's redirect them to the search page
-				header("Location: ". $this_site. "/index.php?doSearch=true&search_query=jam&search_type=ALL");
-			break;
-		case "json":
-		  $jt = array(); $jn = array();
-		  foreach ($tracks as $t) {
-		    $n = array();
-
-		    $meta = $t->getMeta();
-		    
-		    $album = $artist = $genre = false;
-		    $album = $t->getAncestor("album");
-		    if ($album) $artist = $album->getAncestor("artist");
-		    if ($artist) $genre = $artist->getParent();
-		    
-		    // Now let's display
-		    $n['name'] = $meta['title'];
-		    $n['album'] = ($album) ? $album->getName() : '';
-		    $n['artist'] = ($artist) ? $artist->getName() : '';
-		    $n['genre'] = ($genre) ? $genre->getName() : '';
-		    $n['playlink'] = $this_site.$t->getPlayHREF();
-		    $n['metadata'] = $meta;
-
-		    $jt[] = $n;
-		  }
-
-		  foreach ($nodes as $n) {
-		    $a = array();
-		    $art = $n->getMainArt();
-
-		    $a['name']=$n->getName();
-		    $a['type']=$n->getPType();
-		    $a['link']=$this_site.$display->link($node,false,false,false,true,true);
-		    $a['album']=(empty($album)) ? '' : $album->getName();
-		    $a['artist']=(empty($artist))?'':$artist->getName();
-		    $a['image']=($art) ? $display->returnImage($art,false,false, false, "limit", false, false, false, false, false, "0", false, true, true) : '';
-		    $art = $n->getMainArt('75x75');
-		    $a['image']=($art) ? $display->returnImage($art,false,75, 75, "limit", false, false, false, false, false, "0", false, true, true) : '';
-		    $a['playlink'] = $this_site.$n->getPlayHREF();
-
-		    $jn[] = $a;
-		  }
-
-
-		  echo json_encode(array('tracks'=>$jt,'nodes'=>$jn));
-		  break;
-
-		case 'plain':
-		case 'text':
-		  foreach ($tracks as $t) {
-		    echo $t->getName() . "\n";
-		  }
-
-		  foreach ($nodes as $n) {
-		    echo $n->getName() . "\n";
-		  }
+		if (!isset($_REQUEST['jz_path'])) {
+		  return home(); // or browse root?
 		}
+
+		$type = getFormatFromRequest();	
+		$root = new JzMediaNode($_GET['jz_path']);
+		$results = $root->getSubNodes("both");
+		print_results($results,$type);
 	}
 	
+        /**
+	 * Get an api-centric 'homepage' for Jinzora
+	 *
+	 * @author Ben Dodson
+	 * @since 6/21/08
+	 */
+         function home() {
+	   global $api_page;
+	   $entries = array(
+			    array('name' => 'Browse Genres',
+				  'description' => 'See music by genre.',
+				  'browse' => $api_page.'&request=browse&jz_path='.urlencode('/')
+				  ),
+			    array('name' => 'Recently Added Albums',
+				  'description' => 'Albums recently added to Jinzora.',
+				  'browse' => $api_page.'&request=chart&chart=newalbums'
+				  ),
+			    array('name' => 'Recently Played Albums',
+				  'description' => 'Albums recently listened to.',
+				  'browse' => $api_page.'&request=chart&chart=recentplayalbum'
+				  )
+			    );
+
+	   $type = getFormatFromRequest();
+	   print_lists($entries,$type);
+
+	 }
+
+         /**
+	  * Gets the requested chart.
+	  * @author Ben Dodson
+	  * @since 6/21/08
+	  */
+         function chart() {
+	   // todo: use common code for charts in blocks.php and here.
+	   $chart = $_REQUEST['chart'];
+	   $limit = 25;
+	   $results = array();
+	   $root = new jzMediaNode();
+	   switch ($chart) {
+	   case 'newalbums':
+	     $results = $root->getRecentlyAdded('nodes',distanceTo('album',$root),$limit);
+	     break;
+	   case 'recentplayalbum':
+	     $results = $root->getRecentlyPlayed('nodes',distanceTo('album',$root),$limit);
+	     break;
+	   }
+	   
+	   print_results($results,getFormatFromRequest());
+	 }
+
 	/**
 	* 
 	* Echos out the XML header information
@@ -720,38 +662,211 @@
 		echoXMLFooter();
 	}
 	
+/**
+ * Prints out a given array of associative
+ * arrays in the requested format.
+ * 
+ * @author Ben Dodson
+ * @since 6/21/08
+ */
+function print_lists($results, $format='xml') {
+  switch (strtolower($format)) {
+  case 'xml':
+    echoXMLHeader();
+    echo '  <browse>';
+    foreach ($results as $r) {
+      echo '<list>';
+      foreach ($r as $key => $val) {
+	echo "    <${key}>".xmlentities($val)."</${key}>";
+      }
+      echo '</list>1';
+    }
+    echo '  </browse>';
+    echoXMLFooter();
+    break;
+  case 'json':
+    echo json_encode($results);
+    break;
+  }
+}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-?>
+/**
+ * Results is an array of nodes and tracks.
+ * Prints results in a variety of formats.
+ */
+function print_results($results, $format='xml') {
+  global $this_site,$api_page;
+  $display = new jzDisplay();
+		$tracks = array();
+		$nodes = array();
+		// Now let's break our nodes and tracks out
+		foreach ($results as $val) {
+			// We look at objects as leafs or nodes
+			// Leafs are the last branch on the tree
+			// So those would be tracks/videos
+			if ($val->isLeaf()) {
+				$tracks[] = $val;	
+			} else {
+				// Nodes are everything above the leafs
+				// So albums, artists, and genres
+				$nodes[] = $val;
+			}
+		}
+
+		// Now let's output
+		switch ($format){
+		case "xml": 
+				echoXMLHeader();
+				echo "  <search>\n";
+				echo "    <tracks>\n";
+				// Now let's display the tracks
+				if (sizeof($tracks) > 0)
+				foreach($tracks as $track){
+					// Let's get all the data for display
+					// The getMeta function lets us get all the metadata (length, bitrate, etc) from a tack
+					$meta = $track->getMeta();
+					
+					// Now we go up from this item to get it's "ancestors" 
+					// The reason we do this is to make sure we get the right thing
+					// for it, not just the one above.  This is important when using multidisk
+					// albums where their parent would be DISC1 not AlbumName
+					// You can do this recursively if you want
+					$album = $track->getAncestor("album");
+					$artist = $album->getAncestor("artist");
+					$genre = $artist->getParent();
+					
+					// Now let's display
+					echo "      <track>\n";
+					echo "        <name>". xmlentities($meta['title']). "</name>\n";
+					echo "        <metadata>\n";
+					echo "          <filename>". xmlentities($meta['filename']). "</filename>\n";
+					echo "          <tracknumber>". xmlentities($meta['number']). "</tracknumber>\n";
+					echo "          <length>". xmlentities($meta['length']). "</length>\n";
+					echo "          <bitrate>". xmlentities($meta['bitrate']). "</bitrate>\n";
+					echo "          <samplerate>". xmlentities($meta['frequency']). "</samplerate>\n";
+					echo "          <filesize>". xmlentities($meta['size']). "</filesize>\n";
+					echo "        </metadata>\n";
+					echo "        <album>". xmlentities($album->getName()). "</album>\n";
+					echo "        <artist>". xmlentities($artist->getName()). "</artist>\n";
+					echo "        <genre>". xmlentities($genre->getName()). "</genre>\n";
+					echo "        <path>". xmlentities($track->getPath("string")). "</path>\n";
+					echo "        <playlink>". xmlentities($this_site.$track->getPlayHREF()). "</playlink>\n";
+					echo "      </track>\n";
+				}
+				echo "    </tracks>\n";
+				echo "    <nodes>\n";
+				// Now let's display the nodes
+				if (sizeof($nodes) > 0)
+				foreach($nodes as $node){
+					// We do the same things here by getting item off the node
+					// $art would be the image for the item we're looking at
+					// In this case we want the art for the match we found
+					// This works on ALL objects if they have art
+					$art = $node->getMainArt();
+					
+					$album = $node->getAncestor("album");
+					if ($album) {
+						$artist = $album->getAncestor("artist");
+					}
+					
+					echo "      <node>\n";
+					echo "        <name>". xmlentities($node->getName()). "</name>\n";
+					echo "        <type>". xmlentities(ucwords($node->getPType())). "</type>\n";
+					echo "        <link>". xmlentities($this_site.$display->link($node,false,false,false,true,true)). "</link>\n";
+					if (!empty($album)) {
+					  echo "        <album>". xmlentities($album->getName()). "</album>\n";
+					}
+					if (!empty($artist)) {
+					  echo "        <artist>". xmlentities($artist->getName()). "</artist>\n";
+					}
+					echo "        <image>";
+					if ($art){
+						echo xmlentities($display->returnImage($art,false,false, false, "limit", false, false, false, false, false, "0", false, true, true));
+					}
+					echo "        </image>\n"; 
+					echo "        <thumbnail>";
+					$art = $node->getMainArt('75x75');
+					if ($art){
+						echo xmlentities($display->returnImage($art,false,75,75, "limit", false, false, false, false, false, "0", false, true, true));
+					}
+					echo "        </thumbnail>\n"; 
+					if ($node->getPType() == 'artist' || $node->getPType() == 'genre') {
+					  echo "        <playlink>". xmlentities($this_site.$node->getPlayHREF(true,50)). "</playlink>\n";
+					} else {
+					  echo "        <playlink>". xmlentities($this_site.$node->getPlayHREF()). "</playlink>\n";
+					}
+					echo "        <path>". xmlentities($node->getPath("string")). "</path>\n";
+					echo "        <browse>". xmlentities($api_page.'&request=browse&jz_path='.urlencode($node->getPath('string'))). "</browse>\n";
+					echo "      </node>\n";
+				}
+				echo "    </nodes>\n";
+				echo "  </search>\n";
+				echoXMLFooter();
+			break;
+			case "display":
+				// Ok, let's redirect them to the search page
+				header("Location: ". $this_site. "/index.php?doSearch=true&search_query=jam&search_type=ALL");
+			break;
+		case "json":
+		  $jt = array(); $jn = array();
+		  foreach ($tracks as $t) {
+		    $n = array();
+
+		    $meta = $t->getMeta();
+		    
+		    $album = $artist = $genre = false;
+		    $album = $t->getAncestor("album");
+		    if ($album) $artist = $album->getAncestor("artist");
+		    if ($artist) $genre = $artist->getParent();
+		    
+		    // Now let's display
+		    $n['name'] = $meta['title'];
+		    $n['album'] = ($album) ? $album->getName() : '';
+		    $n['artist'] = ($artist) ? $artist->getName() : '';
+		    $n['genre'] = ($genre) ? $genre->getName() : '';
+		    $n['playlink'] = $this_site.$t->getPlayHREF();
+		    $n['metadata'] = $meta;
+		    $n['path'] = $t->getPath("string");
+
+		    $jt[] = $n;
+		  }
+
+		  foreach ($nodes as $n) {
+		    $a = array();
+		    $art = $n->getMainArt();
+
+		    $a['name']=$n->getName();
+		    $a['type']=ucwords($n->getPType());
+		    $a['link']=$this_site.$display->link($node,false,false,false,true,true);
+		    $a['album']=(empty($album)) ? '' : $album->getName();
+		    $a['artist']=(empty($artist))?'':$artist->getName();
+		    $a['image']=($art) ? $display->returnImage($art,false,false, false, "limit", false, false, false, false, false, "0", false, true, true) : '';
+		    $art = $n->getMainArt('75x75');
+		    $a['image']=($art) ? $display->returnImage($art,false,75, 75, "limit", false, false, false, false, false, "0", false, true, true) : '';
+		    if ($a['type']=='Artist' || $a['type'] == 'Genre') {
+		      $a['playlink'] = $this_site.$n->getPlayHREF(true,50);
+		    } else {
+		      $a['playlink'] = $this_site.$n->getPlayHREF();
+		    }
+		    $a['path'] = $n->getPath("string");
+		    $a['browse'] = $api_page.'&request=browse&jz_path='.urlencode($n->getPath('string'));
+
+		    $jn[] = $a;
+		  }
+
+
+		  echo json_encode(array('tracks'=>$jt,'nodes'=>$jn));
+		  break;
+
+		case 'plain':
+		case 'text':
+		  foreach ($tracks as $t) {
+		    echo $t->getName() . "\n";
+		  }
+
+		  foreach ($nodes as $n) {
+		    echo $n->getName() . "\n";
+		  }
+		}
+
+}
