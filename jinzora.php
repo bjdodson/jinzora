8e0c9d1b26ab1d326fd1c394baefd0fb151743e1
Ben Dodson <bjdodson@gmail.com>
2010-02-07 13:58:43 -0500

    formatting

--- jinzora.php
+++ jinzora.php
@@ -1,4 +1,5 @@
-<?php // define('JZ_SECURE_ACCESS','true');
-	$_SERVER['cms_type'] = "mambo";
-	include_once('components/com_jinzora/index.php');
+<?php   
+  // define('JZ_SECURE_ACCESS','true');
+  $_SERVER['cms_type'] = "mambo";
+  include_once('components/com_jinzora/index.php');
 ?>
